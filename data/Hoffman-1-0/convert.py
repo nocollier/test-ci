8d6230113a3b0a7447b4fca0c38b57b34415b0da
Nathan Collier <nathaniel.collier@gmail.com>
2026-02-24 14:08:36 -0500

    added some stuff

---
+++ data/Hoffman-1-0/convert.py
@@ -0,0 +1,13 @@
+import requests
+
+# Some fake work, let's assume here that downloading the raw data IS formatting it too.
+remote_source = "https://www.ilamb.org/ILAMB-Data/DATA/nbp/HOFFMAN/nbp_1850-2010.nc"
+resp = requests.get(remote_source, stream=True)
+resp.raise_for_status()
+
+CHUNKSIZE = 2**20
+local_source = "nbp_1850-2010.nc"
+with open(local_source, "wb") as fdl:
+    for chunk in resp.iter_content(chunk_size=CHUNKSIZE):
+        if chunk:
+            fdl.write(chunk)
